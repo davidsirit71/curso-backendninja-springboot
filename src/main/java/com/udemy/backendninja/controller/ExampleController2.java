2101634b006dc992eecf530109f893e0888d741b
David Sirit <davidsirit@yahoo.com>
2019-08-07 22:19:16 +0200

    desde casa componentes

--- src/main/java/com/udemy/backendninja/controller/ExampleController2.java
+++ src/main/java/com/udemy/backendninja/controller/ExampleController2.java
@@ -3,12 +3,15 @@ package com.udemy.backendninja.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.udemy.backendninja.component.ExampleComponent;
 import com.udemy.backendninja.model.Person;
 
 @Controller
@@ -17,10 +20,15 @@ public class ExampleController2 {
 	
 	public static final String EXAMPLE_VIEW = "example"; 
 	
+	@Autowired							// indica a spring que inyectamos un componente en memoria
+	@Qualifier("exampleComponent")		//indica a spring nombre del beam que esta en su memoria tal como esta en @Component
+	private ExampleComponent exampleComponent;
+	
 	// Primera forma de retornar plantilla: usada cuando se insertan pocos datos
 	
 	@GetMapping("/exampleString") 
 	public String exampleString(Model model) {
+		exampleComponent.sayHello();
 		model.addAttribute("people", getPeople());
 		return EXAMPLE_VIEW;  
 	}
